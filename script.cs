132c22f9df5de6967368e0e11043d0fda6dee805
Civabcici <civabcic58@gmail.com>
2025-09-29 21:27:03 +0500

    -

--- script.cs
+++ script.cs
@@ -10,14 +10,25 @@ class Program
         int result = Convert.ToInt32(number, 2);
 
         Console.WriteLine($"Десятичное число: {result}");
+
         //2.Написать переводчик из десятичной в двоичную(Ввод: целочисленное число, Вывод: строка, типа "11011100")
         Console.Write("Введите десятичное число: ");
         int num = int.Parse(Console.ReadLine());
         string res = Convert.ToString(num, 2);
 
         Console.WriteLine($"Двоичное число: {res}");
-        //3. 
 
+        //3. Сегодня в N часов М минут хозяин магазина заказал доставку нового товара. Оператор сказал, что продукты доставят через Т минут. Сколько будет времени на электронных часах, когда привезут долгожданные продукты ?
+        int N = int.Parse(Console.ReadLine()); 
+        int M = int.Parse(Console.ReadLine());  
+        int T = int.Parse(Console.ReadLine());  
+
+        int totalMinutes = N * 60 + M + T;
+
+        int hours = (totalMinutes / 60) % 24;
+        int minutes = totalMinutes % 60;
+
+        Console.WriteLine($"{hours:D2}:{minutes:D2}");
 
 
     }
